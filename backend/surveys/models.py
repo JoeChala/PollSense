76adecf6d539f214400b6bef99bfaf0c4f6ff73d
jchalakkal16+github@gmail.com <jchalakkal16@gmail.com>
2026-03-24 21:28:55 +0530

    feat(survey): remade models for survey

--- backend/surveys/models.py
+++ backend/surveys/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.core.exceptions import ValidationError
 import uuid
 
 
@@ -10,8 +11,16 @@ class Survey(models.Model):
         PUBLISHED = "PUBLISHED", "Published"
         CLOSED = "CLOSED", "Closed"
         ARCHIVED = "ARCHIVED", "Archived"
-    
-    survey_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    # contains current visibility options
+    class SurveyVisibility(models.TextChoices):
+        PUBLIC = "PUBLIC", "Public"
+        PRIVATE = "PRIVATE", "Private"
+
+    survey_id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4, 
+        editable=False, 
+        unique=True)
 
     # default user model
     owner = models.ForeignKey(
@@ -33,7 +42,15 @@ class Survey(models.Model):
         default=SurveyStatus.DRAFT
     )
 
+    visibility = models.CharField(
+        max_length=20,
+        choices=SurveyVisibility.choices,
+        default=SurveyVisibility.PRIVATE
+    )
+
     created_at = models.DateTimeField(auto_now_add=True)
+
+    updated_at = models.DateTimeField(auto_now=True)
 
 
 class Question(models.Model):
@@ -54,22 +71,45 @@ class Question(models.Model):
         default=QuestionTypes.TEXT
     )
     # stores the order of the current question in the survey
-    order = models.IntegerField(default=1)
+    order = models.PositiveIntegerField(default=1)
 
-class MCQChoice(models.Model):
-    question = models.ForeignKey(Question, related_name="mcq_choices",on_delete=models.CASCADE)
+    is_required = models.BooleanField(default=True)
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('survey', 'order')
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, related_name="choices",on_delete=models.CASCADE)
+    
     text = models.CharField(max_length=255,blank=False)
 
-class Response(models.Model):
-    survey = models.ForeignKey(Survey,on_delete=models.CASCADE,related_name="responses")
-    submitted_at = models.DateTimeField(auto_now_add=True)
-    session_key = models.CharField(max_length=40)
+    order = models.PositiveIntegerField(default=1)
 
-class MCQAnswer(models.Model):
+    class Meta:
+        unique_together = ('question','order')
+
+class Response(models.Model):
+    survey = models.ForeignKey(Survey,on_delete=models.CASCADE,related_name="responses",db_index=True)
+
+    submitted_at = models.DateTimeField(auto_now_add=True,db_index=True)
+
+    metadata = models.JSONField(null=True, blank=True)
+
+class Answer(models.Model):
     response = models.ForeignKey(Response, on_delete=models.CASCADE, related_name="answers")
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE,db_index=True)
 
     text_answer = models.TextField(null=True, blank=True)
-    selected_choice = models.ForeignKey(MCQChoice, null=True, blank=True, on_delete=models.SET_NULL)
+    selected_choice = models.ForeignKey(Choice, null=True, blank=True, on_delete=models.SET_NULL)
     rating_value = models.IntegerField(null=True, blank=True)
+
+    def clean(self):
+        if self.question.question_type == Question.QuestionTypes.MCQ and not self.selected_choice:
+            raise ValidationError("MCQ requires selected_choice")
+
+        if self.question.question_type == Question.QuestionTypes.TEXT and not self.text_answer:
+            raise ValidationError("Text question requires text_answer")
 
--- backend/surveys/urls.py
+++ backend/surveys/urls.py
@@ -1,10 +1,7 @@
-from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path(route='auth/signin/',view=views.user_signin),
-    path(route='surveys/',view=views.survey_list),
+    path(route='',view=views.survey_list),
     path(route='s/<uuid:survey_id>/',view=views.take_survey)
 ]
